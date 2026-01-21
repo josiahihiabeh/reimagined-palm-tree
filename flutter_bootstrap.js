32a216f1fde4dc0777e2631497deec898751655b
josiahihiabeh <ihiabehojodalejosiah@gmail.com>
2026-01-21 11:41:14 +0100

    Add Google Sign In

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2702779839"
+    serviceWorkerVersion: "2015814439"
   }
 });
